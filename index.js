c8994694017dd6ef2dbbacf75f081329b21e1516
Tom MacWright <tom@macwright.org>
2015-05-15 14:32:36 -0400

    Simplify

--- index.js
+++ index.js
@@ -1,32 +1,9 @@
-var Dispatcher = require('flux').Dispatcher,
-  xtend = require('xtend/mutable');
+var Dispatcher = require('flux').Dispatcher;
 
 /**
  * This is a singleton: all modules that require this dispatcher will
  * receive the same instance.
  */
-var AppDispatcher = xtend(new Dispatcher(), {
-  /**
-   * A bridge function between the views and the dispatcher, marking the action
-   * as a view action.  Another variant here could be handleServerAction.
-   * @param {object} action The data coming from the view.
-   */
-  handleViewAction: function(action) {
-    this.dispatch({
-      source: 'VIEW_ACTION',
-      action: action
-    });
-  },
-
-  /**
-   * @param {object} action The data coming from the view.
-   */
-  handleServerAction: function(action) {
-    this.dispatch({
-      source: 'SERVER_ACTION',
-      action: action
-    });
-  }
-});
+var AppDispatcher = new Dispatcher();
 
 module.exports = AppDispatcher;
